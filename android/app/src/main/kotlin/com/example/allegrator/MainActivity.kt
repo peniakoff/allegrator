ac9651cce5197a407c6e310b26d013c4a1e43e0c
Tomasz Miller <tomasz.miller@outlook.com>
2024-10-04 09:57:34 +0200

    Update for Flutter initial project

---
+++ android/app/src/main/kotlin/com/example/allegrator/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.allegrator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/tomasz/Libs/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/tomasz/Projekty/Github/allegrator"
+export "FLUTTER_APPLICATION_PATH=/Users/tomasz/Projekty/allegrator"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
